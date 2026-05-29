f6b82d93caf4840cd35883f948681ba4082d34e6
dexolaretxyz <dexolaretxyz@gmail.com>
2026-05-29 19:51:46 +0100

    fix: remove Sentry config blocking production routes

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,23 +1,7 @@
-import { withSentryConfig } from '@sentry/nextjs';
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default withSentryConfig(nextConfig, {
-  org: "xavier-300",
-  project: "xavier-300-web",
-  silent: !process.env.CI,
-  widenClientFileUpload: true,
-  tunnelRoute: "/monitoring",
-  telemetry: false,
-  sourcemaps: {
-    disable: true,
-  },
-  webpack: {
-    reactComponentAnnotation: { enabled: true },
-    treeshake: { removeDebugLogging: true },
-    automaticVercelMonitors: true,
-  }
-});
+export default nextConfig;
