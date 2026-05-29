e2765176ee9cb60132641a2f06d6cc2b716d431c
dexolaretxyz <dexolaretxyz@gmail.com>
2026-05-29 22:40:16 +0100

    fix: correctly export next config to resolve 404

---
+++ next.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Any specific configs you had before (like Turbopack or image domains) go inside these brackets
+};
+
+module.exports = nextConfig;
